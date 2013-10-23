255938b16855f3e378519fa0cb2191673f7074af
galadhremmin <zanathel@gmail.com>
2013-10-24 00:54:32 +0200

    Ordered files into folders according to their function.

---
+++ leowic-3_Labb3b/Controllers/Opportunities/AldOpportunitySelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  AldOpportunitySelectionViewController.h
+//  leowic-3_Labb3b
+//
+//  Created by Leonard Wickmark on 10/23/13.
+//  Copyright (c) 2013 LTU. All rights reserved.
+//
+
+#import "AldAlphabeticSectionsTableViewController.h"
+
+@interface AldOpportunitySelectionViewController : AldAlphabeticSectionsTableViewController<UISearchBarDelegate>
+
+@end
---
+++ leowic-3_Labb3b/Controllers/Opportunities/AldOpportunitySelectionViewController.m
@@ -0,0 +1,15 @@
+//
+//  AldOpportunitySelectionViewController.m
+//  leowic-3_Labb3b
+//
+//  Created by Leonard Wickmark on 10/23/13.
+//  Copyright (c) 2013 LTU. All rights reserved.
+//
+
+#import "AldOpportunitySelectionViewController.h"
+
+@implementation AldOpportunitySelectionViewController
+
+
+
+@end
